ee2bccb2496582e6a8466ff8ae8c080c11d0ad05
Ahmed Esam <97251566+ahmedeesam@users.noreply.github.com>
2022-01-06 21:42:37 +0200

    Add files via upload

---
+++ ext_int.c
@@ -0,0 +1,22 @@
+﻿/*
+ * ext_int.c
+ *
+ * Created: 14/12/2021 05:01:39 م
+ *  Author: ahmed essam
+ */ 
+
+#include "ext_int.h"
+
+void int0_init()
+{
+	//DDRD &= ~(1<<PORTD2);
+	EICRA |= (1<<ISC01);
+	EIMSK |= (1<<INT0);
+}
+
+void int1_init()
+{
+	//DDRD &= ~(1<<PORTD3);
+	EICRA |= ((1<<ISC11)|(1<<ISC10));
+	EIMSK |= (1<<INT1);
+}
